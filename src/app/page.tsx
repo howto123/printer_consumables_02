4c05bc8e75c5b2a76ce2e3daf983ef059ee21e88
me <me>
2023-09-11 15:55:33 +0200

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,94 +2,46 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className={styles.main}>
+			<h1>
+				Welcome - thanks for caring about your colleagues.
+			</h1>
+			<div>
+				<div>
+					<h2>Where exactly is your printer?</h2>
+					<div>
+						Here will be the functionality to choose locations.
+					</div>
+				</div>
+				<div>
+					<h2>What printer is it?</h2>
+					<div>
+						Printers relative to location.
+					</div>
+				</div>
+				<div>
+					<h2>What printer model is it?</h2>
+					<div>
+						Printers relative to location.
+					</div>
+				</div>
+				<div>
+					<h2>What consumable needs to be ordered?</h2>
+					<div>
+						Consumables, typically toner of a specific color relative to printer model.
+					</div>
+				</div>
+				<div>
+					<h2>Current selection</h2>
+					<div>
+						Summary
+					</div>
+					<div>
+						&quot;Ok&quot; or &quot;place order&quot; button
+					</div>
+				</div>
+			</div>
+		</main>
+	)
 }
